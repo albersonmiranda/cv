29d9ad8db870c31940065d4e329bd9e79585a2fa
Alberson Miranda <albersonmiranda@hotmail.com>
2025-05-20 23:07:51 -0300

    update resume

--- old/cv.R
+++
@@ -1,7 +0,0 @@
-datadrivencv::use_datadriven_cv(
-  full_name = "Alberson Miranda",
-  data_location = "https://docs.google.com/spreadsheets/d/1RTBYV09vzWl6mr6MVUVb81V8b1G2KHDP0C3frcBqXyQ/edit?usp=sharing",
-  pdf_location = "https://github.com/albersonmiranda/cv/raw/master/miranda_cv.pdf",
-  source_location = "https://github.com/albersonmiranda/cv",
-  use_network_logo = FALSE
-)
